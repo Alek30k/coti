b1bd7825a41fec3f51f79d0553aded4ba7f2f904
Alek30k <alemank23k@gmail.com>
2023-11-03 22:28:30 -0300

    last commit

--- src/componentes/DataFetchContext.jsx
+++ src/componentes/DataFetchContext.jsx
@@ -14,7 +14,7 @@ const DataFetchProvider = ({ children }) => {
   });
 
   useEffect(() => {
-    fetch("datos.json")
+    fetch("./datos.json")
       .then((response) => {
         if (response.ok) {
           return response.json();
